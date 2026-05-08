f704593215165a49ddf441f78b6e0bc05378652f
Guilhermehriq <contatoguilhermehrp@gmail.com>
2026-05-08 17:00:54 -0300

    feat: implement transaction form and dynamic dashboard updates

--- src/main/java/com/guilherme/controlefinanceiro/controller/TransacaoController.java
+++ src/main/java/com/guilherme/controlefinanceiro/controller/TransacaoController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class TransacaoController {
 
@@ -32,4 +34,5 @@ public class TransacaoController {
     public double saldo(){
         return service.calcularSaldo();
     }
+
 }
